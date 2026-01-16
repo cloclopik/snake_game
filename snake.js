952cf51017b4d673fefd4188861b76689546d47b
cloclopik <clovis.darmengeat@gmail.com>
2026-01-16 15:11:39 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,47 +1,81 @@
-let snakeCanvas;
-let snakeCtx;
-let snake;
-let food;
-let direction;
-let snakeInterval;
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-function startSnake() {
-    snakeCanvas = document.getElementById("gameCanvas");
-    snakeCtx = snakeCanvas.getContext("2d");
+const box = 25;
+let snake, direction, food, gameInterval;
 
-    snake = [{ x: 10, y: 10 }];
-    food = spawnFood();
+function startSnakeGame() {
+    clearInterval(gameInterval);
+
+    snake = [{ x: 10 * box, y: 10 * box }];
     direction = "RIGHT";
 
-    clearInterval(snakeInterval);
-    snakeInterval = setInterval(updateSnake, 120);
+    food = spawnFood();
 
-    document.addEventListener("keydown", changeDirection);
+    document.onkeydown = changeDirection;
+
+    gameInterval = setInterval(drawSnake, 120);
 }
 
-function updateSnake() {
-    const head = { ...snake[0] };
+function spawnFood() {
+    let pos;
+    do {
+        pos = {
+            x: Math.floor(Math.random() * 20) * box,
+            y: Math.floor(Math.random() * 20) * box
+        };
+    } while (snake.some(s => s.x === pos.x && s.y === pos.y));
+    return pos;
+}
 
-    if (direction === "UP") head.y--;
-    if (direction === "DOWN") head.y++;
-    if (direction === "LEFT") head.x--;
-    if (direction === "RIGHT") head.x++;
+function changeDirection(event) {
+    const key = event.keyCode;
+    if (key === 37 && direction !== "RIGHT") direction = "LEFT";
+    if (key === 38 && direction !== "DOWN") direction = "UP";
+    if (key === 39 && direction !== "LEFT") direction = "RIGHT";
+    if (key === 40 && direction !== "UP") direction = "DOWN";
+}
+
+function drawSnake() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Grille verte
+    for (let x = 0; x < canvas.width; x += box) {
+        for (let y = 0; y < canvas.height; y += box) {
+            ctx.fillStyle = (x / box + y / box) % 2 === 0 ? "#6aaa64" : "#4e8234";
+            ctx.fillRect(x, y, box, box);
+        }
+    }
+
+    // Fruit
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(food.x + box / 2, food.y + box / 2, box / 2.5, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Snake
+    snake.forEach((part, index) => {
+        ctx.fillStyle = index === 0 ? "#00ffcc" : "#00aa00";
+        ctx.fillRect(part.x, part.y, box, box);
+    });
+
+    let head = { ...snake[0] };
+
+    if (direction === "LEFT") head.x -= box;
+    if (direction === "UP") head.y -= box;
+    if (direction === "RIGHT") head.x += box;
+    if (direction === "DOWN") head.y += box;
 
     // Collision mur
     if (
         head.x < 0 || head.y < 0 ||
-        head.x >= 30 || head.y >= 30
+        head.x >= canvas.width || head.y >= canvas.height ||
+        snake.some(s => s.x === head.x && s.y === head.y)
     ) {
-        gameOver();
+        clearInterval(gameInterval);
+        alert("Game Over");
+        location.reload();
         return;
-    }
-
-    // Collision corps
-    for (let part of snake) {
-        if (part.x === head.x && part.y === head.y) {
-            gameOver();
-            return;
-        }
     }
 
     snake.unshift(head);
@@ -51,40 +85,4 @@ function updateSnake() {
     } else {
         snake.pop();
     }
-
-    drawSnake();
-}
-
-function drawSnake() {
-    snakeCtx.clearRect(0, 0, 600, 600);
-
-    // Snake
-    snakeCtx.fillStyle = "lime";
-    snake.forEach(p =>
-        snakeCtx.fillRect(p.x * 20, p.y * 20, 20, 20)
-    );
-
-    // Food
-    snakeCtx.fillStyle = "red";
-    snakeCtx.fillRect(food.x * 20, food.y * 20, 20, 20);
-}
-
-function spawnFood() {
-    return {
-        x: Math.floor(Math.random() * 30),
-        y: Math.floor(Math.random() * 30)
-    };
-}
-
-function changeDirection(e) {
-    if (e.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
-    if (e.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
-    if (e.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
-    if (e.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
-}
-
-function gameOver() {
-    clearInterval(snakeInterval);
-    alert("Game Over !");
-    location.reload();
 }
