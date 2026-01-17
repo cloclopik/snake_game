0869c4b2f1ac596cd4c144c96f4269561d789244
cloclopik <clovis.darmengeat@gmail.com>
2026-01-17 20:44:47 +0100

    Update save.js

--- save.js
+++ save.js
@@ -1,1 +1,11 @@
+let gold = parseInt(localStorage.getItem("gold")) || 0;
+
+function addGold(amount) {
+    gold += amount;
+    localStorage.setItem("gold", gold);
+}
+
+function getGold() {
+    return gold;
+}
 
